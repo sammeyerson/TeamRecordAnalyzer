af1ecb64d549a688787afbec1d873613296566a7
SamMeyerson <sammeyerson@ufl.edu>
2020-02-12 11:59:56 -0500

    Finds record of team when player plays

--- analyze.py
+++ analyze.py
@@ -9,8 +9,22 @@ playerdata=pd.read_csv('PlayerInfo/Kyrie.csv')
 teamdf=teamdata.iloc[:,7:14]
 #print(teamdf)
 #print(playerdata)
-teamGames=teamdata[['G','result','Tm','Opp']]
+teamGames=teamdata[['Rk','result','Tm','Opp']]
 gamesPlayed=playerdata[['Rk','G']]
 gamesPlayed=gamesPlayed[gamesPlayed['G']>0]
-print(gamesPlayed)
-print(teamGames)
+#print(gamesPlayed)
+#print(teamGames)
+
+commonGames = pd.merge(teamGames, gamesPlayed, on=['Rk'], how='inner')
+print(commonGames)
+itt=int(0)
+winCount=int(0)
+lossCount=int(0)
+for index, row in commonGames.iterrows():
+
+    if(row['result']=='W'):
+        winCount=winCount+1
+    else:
+        lossCount=lossCount+1
+    itt=itt+1
+print(winCount,'-',lossCount)
