2395973ccbde3cda84cc6c2024941d1a93dba273
SamMeyerson <sammeyerson@ufl.edu>
2020-02-17 15:01:00 -0500

    added  more stuff, ppg, function for SOS

--- analyze.py
+++ analyze.py
@@ -4,9 +4,10 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+from findRecord import findRecord
 
-teamdata=pd.read_csv('TeamRecords/NetsRecordInfo.csv')
-playerdata=pd.read_csv('PlayerInfo/Kyrie.csv')
+teamdata=pd.read_csv('TeamRecords/BucksRecordInfo.csv')
+playerdata=pd.read_csv('PlayerInfo/Giannis.csv')
 teamdf=teamdata.iloc[:,7:14]
 #print(teamdf)
 #print(playerdata)
@@ -27,14 +28,21 @@ uncommonGames= pd.merge(teamGames, gamesMissed, on=['Rk'])
 itt=int(0)
 winCount=int(0)
 lossCount=int(0)
+
+pointsFor=int(0)
+pointsAgainst=int(0)
 for index, row in commonGames.iterrows():
 
     if(row['result']=='W'):
         winCount=winCount+1
+
     else:
         lossCount=lossCount+1
+    pointsFor=pointsFor+int(row['Tm'])
     itt=itt+1
 print('Record with player: ',winCount,'-',lossCount)
+print('PPG with player: ',pointsFor/itt)
+
 
 #print(uncommonGames)
 itt=int(0)
@@ -46,5 +54,8 @@ for index, row in uncommonGames.iterrows():
         winCount=winCount+1
     else:
         lossCount=lossCount+1
+    pointsAgainst=pointsAgainst+int(row['Tm'])
     itt=itt+1
 print('Record without player: ',winCount,'-',lossCount)
+print('PPG without player: ', pointsAgainst/itt)
+#print(findRecord('hi'))
---
+++ findRecord.py
@@ -0,0 +1,13 @@
+import csv
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+def findRecord(team):
+
+    totalTeamData=pd.read_csv('TeamRecords/AllTeamsInfo.csv')
+
+
+
+    return totalTeamData
