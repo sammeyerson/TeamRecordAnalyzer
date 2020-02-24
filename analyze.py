cb84da34c515662588e496e3dfec6521e7d6ef2f
SamMeyerson <sammeyerson@ufl.edu>
2020-02-23 22:15:06 -0500

    Using web scraping to find record with/without player

--- analyze.py
+++ analyze.py
@@ -5,57 +5,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 from findRecord import findRecord
+from findRecord import get_team_misc, get_game_logs, get_stats, \
+get_single_season_stats, get_team_stats, get_opp_stats, get_schedule, \
+get_team_schedule, record_with_player, record_without_player
 
-teamdata=pd.read_csv('TeamRecords/BucksRecordInfo.csv')
-playerdata=pd.read_csv('PlayerInfo/Giannis.csv')
-teamdf=teamdata.iloc[:,7:14]
-#print(teamdf)
-#print(playerdata)
-teamGames=teamdata[['Rk','result','Tm','Opp']]
+start_date='2019-10-22'
+end_date='2020-04-15'
 
-gamesPlayed=playerdata[['Rk','G']]
-gamesPlayed.replace(to_replace=np.nan, value= 0)
+#print('Record without player: ',winCount,'-',lossCount)
+#print('PPG without player: ', pointsAgainst/itt)
+#print(findRecord('Lakers'))
+#print(get_team_misc('MIA', 2020))
 
-gamesMissed=gamesPlayed[gamesPlayed['G'].isnull()]
-gamesPlayed=gamesPlayed[gamesPlayed['G']>0]
-
-#print(gamesPlayed)
-#print(teamGames)
-
-commonGames = pd.merge(teamGames, gamesPlayed, on=['Rk'], how='inner')
-uncommonGames= pd.merge(teamGames, gamesMissed, on=['Rk'])
-#print(commonGames)
-itt=int(0)
-winCount=int(0)
-lossCount=int(0)
-
-pointsFor=int(0)
-pointsAgainst=int(0)
-for index, row in commonGames.iterrows():
-
-    if(row['result']=='W'):
-        winCount=winCount+1
-
-    else:
-        lossCount=lossCount+1
-    pointsFor=pointsFor+int(row['Tm'])
-    itt=itt+1
-print('Record with player: ',winCount,'-',lossCount)
-print('PPG with player: ',pointsFor/itt)
+#print(get_single_season_stats('James Harden'))
+#print(get_team_stats('MIA',2020))
+#print(get_opp_stats('MIA',2020))
 
 
-#print(uncommonGames)
-itt=int(0)
-winCount=int(0)
-lossCount=int(0)
-for index, row in uncommonGames.iterrows():
-
-    if(row['result']=='W'):
-        winCount=winCount+1
-    else:
-        lossCount=lossCount+1
-    pointsAgainst=pointsAgainst+int(row['Tm'])
-    itt=itt+1
-print('Record without player: ',winCount,'-',lossCount)
-print('PPG without player: ', pointsAgainst/itt)
-print(findRecord('Heat'))
+#print(get_game_logs('LaMarcus Aldridge',start_date,end_date))
+#print(get_team_schedule('Spurs',2020))
+player=input('Enter Player Name(ie: James Harden, Lebron James):')
+team=input('Enter Name of Team(Rockets for James Harden, Lakers for Lebron James):')
+#print(record_with_player('LaMarcus Aldridge', 'Spurs'))
+#print(record_without_player('LaMarcus Aldridge', 'Spurs'))
+print('Record with ',player,': ',record_with_player(player, team))
+print('Record without ',player,': ',record_without_player(player, team))
--- findRecord.py
+++ findRecord.py
@@ -3,6 +3,13 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+from requests import get
+from bs4 import BeautifulSoup
+
+try:
+    from constants import TEAM_TO_TEAM_ABBR
+except:
+    from basketball_reference_scraper.constants import TEAM_TO_TEAM_ABBR
 
 def findRecord(team):
 
@@ -52,14 +59,304 @@ def findRecord(team):
 
         itt=itt+1
 
-    return winPct_frame
+    return 0
 
 
 def strengthOfSchedule(team):
 
-    
+
 
 
 
 
     return 0
+
+
+
+def get_team_misc(team, season_end_year):
+    r = get(f'https://widgets.sports-reference.com/wg.fcgi?css=1&site=bbr&url=%2Fleagues%2FNBA_{season_end_year}.html&div=div_misc_stats')
+    df = None
+    if r.status_code==200:
+        soup = BeautifulSoup(r.content, 'html.parser')
+        table = soup.find('table')
+        df = pd.read_html(str(table))[0]
+        df.columns = list(map(lambda x: x[1], list(df.columns)))
+        league_avg_index = df[df['Team']=='League Average'].index[0]
+        df = df[:league_avg_index]
+        df['Team'] = df['Team'].apply(lambda x: x.replace('*', '').upper())
+        df['TEAM'] = df['Team'].apply(lambda x: TEAM_TO_TEAM_ABBR[x])
+        df = df.drop(['Rk', 'Team'], axis=1)
+        df.rename(columns = {'Age': 'AGE', 'Pace': 'PACE', 'Arena': 'ARENA', 'Attend.': 'ATTENDANCE', 'Attend./G': 'ATTENDANCE/G'}, inplace=True)
+        s = df[df['TEAM']==team]
+        #s['SEASON'] = f'{season_end_year-1}-{str(season_end_year)[2:]}'
+        return pd.Series(index=list(s.columns), data=s.values.tolist()[0])
+
+def get_game_logs(name, start_date, end_date, playoffs=False):
+    try:
+        from utils import get_player_suffix
+    except:
+        from basketball_reference_scraper.utils import get_player_suffix
+
+    suffix = get_player_suffix(name).replace('/', '%2F').replace('.html', '')
+    start_date_str = start_date
+    end_date_str = end_date
+    start_date = pd.to_datetime(start_date)
+    end_date = pd.to_datetime(end_date)
+    years = list(range(start_date.year, end_date.year+2))
+    if playoffs:
+        selector = 'div_pgl_basic_playoffs'
+    else:
+        selector = 'div_pgl_basic'
+    final_df = None
+    for year in years:
+        r = get(f'https://widgets.sports-reference.com/wg.fcgi?css=1&site=bbr&url={suffix}%2Fgamelog%2F{year}&div={selector}')
+        if r.status_code==200:
+            soup = BeautifulSoup(r.content, 'html.parser')
+            table = soup.find('table')
+            if table:
+                df = pd.read_html(str(table))[0]
+                df.rename(columns = {'Date': 'DATE', 'Tm': 'TEAM', 'Unnamed: 5': 'HOME/AWAY', 'Opp': 'OPPONENT',
+                        'Unnamed: 7': 'RESULT', 'GmSc': 'GAME_SCORE'}, inplace=True)
+                df['HOME/AWAY'] = df['HOME/AWAY'].apply(lambda x: 'AWAY' if x=='@' else 'HOME')
+                df = df[df['Rk']!='Rk']
+                df = df.drop(['Rk', 'G','Age'], axis=1)
+                df = df.loc[(df['DATE'] >= start_date_str) & (df['DATE'] <= end_date_str)]
+                active_df = pd.DataFrame(columns = list(df.columns))
+                for index, row in df.iterrows():
+                    #if len(row['GS'])>1:
+                        #ontinue
+                    active_df = active_df.append(row)
+                if final_df is None:
+                    final_df = pd.DataFrame(columns=list(active_df.columns))
+                final_df = final_df.append(active_df)
+    return final_df
+
+
+def get_stats(name, stat_type='PER_GAME', playoffs=False, career=False):
+    try:
+        from utils import get_player_suffix
+    except:
+        from basketball_reference_scraper.utils import get_player_suffix
+    suffix = get_player_suffix(name).replace('/', '%2F')
+    selector = stat_type.lower()
+    if playoffs:
+        selector = 'playoffs_'+selector
+    r = get(f'https://widgets.sports-reference.com/wg.fcgi?css=1&site=bbr&url={suffix}&div=div_{selector}')
+    if r.status_code==200:
+        soup = BeautifulSoup(r.content, 'html.parser')
+        table = soup.find('table')
+        df = pd.read_html(str(table))[0]
+        df.rename(columns={'Season': 'SEASON', 'Age': 'AGE',
+                  'Tm': 'TEAM', 'Lg': 'LEAGUE', 'Pos': 'POS'}, inplace=True)
+        career_index = df[df['SEASON']=='Career'].index[0]
+        if career:
+            df = df.iloc[career_index+2:, :]
+        else:
+            df = df.iloc[:career_index, :]
+
+        df = df.reset_index().dropna(axis=1).drop('index', axis=1)
+        return df
+
+def get_single_season_stats(playerName, stat_type='PER_GAME'):
+
+    careerStats=get_stats(playerName, stat_type)
+    return careerStats.tail(1)
+
+def get_team_stats(team, season_end_year, data_format='PER_GAME'):
+    if data_format=='TOTAL':
+        selector = 'div_team-stats-base'
+    elif data_format=='PER_GAME':
+        selector = 'div_team-stats-per_game'
+    elif data_format=='PER_POSS':
+        selector = 'div_team-stats-per_poss'
+    r = get(f'https://widgets.sports-reference.com/wg.fcgi?css=1&site=bbr&url=%2Fleagues%2FNBA_{season_end_year}.html&div={selector}')
+    df = None
+    if r.status_code==200:
+        soup = BeautifulSoup(r.content, 'html.parser')
+        table = soup.find('table')
+        df = pd.read_html(str(table))[0]
+        league_avg_index = df[df['Team']=='League Average'].index[0]
+        df = df[:league_avg_index]
+        df['Team'] = df['Team'].apply(lambda x: x.replace('*', '').upper())
+        df['TEAM'] = df['Team'].apply(lambda x: TEAM_TO_TEAM_ABBR[x])
+        df = df.drop(['Rk', 'Team'], axis=1)
+        s = df[df['TEAM']==team]
+        return pd.Series(index=list(s.columns), data=s.values.tolist()[0])
+
+
+def get_opp_stats(team, season_end_year, data_format='PER_GAME'):
+    if data_format=='TOTAL':
+        selector = 'div_opponent-stats-base'
+    elif data_format=='PER_GAME':
+        selector = 'div_opponent-stats-per_game'
+    elif data_format=='PER_POSS':
+        selector = 'div_opponent-stats-per_poss'
+    r = get(f'https://widgets.sports-reference.com/wg.fcgi?css=1&site=bbr&url=%2Fleagues%2FNBA_{season_end_year}.html&div={selector}')
+    df = None
+    if r.status_code==200:
+        soup = BeautifulSoup(r.content, 'html.parser')
+        table = soup.find('table')
+        df = pd.read_html(str(table))[0]
+        league_avg_index = df[df['Team']=='League Average'].index[0]
+        df = df[:league_avg_index]
+        df['Team'] = df['Team'].apply(lambda x: x.replace('*', '').upper())
+        df['TEAM'] = df['Team'].apply(lambda x: TEAM_TO_TEAM_ABBR[x])
+        df = df.drop(['Rk', 'Team'], axis=1)
+        df.columns = list(map(lambda x: 'OPP_'+x, list(df.columns)))
+        df.rename(columns={'OPP_TEAM': 'TEAM'}, inplace=True)
+        s = df[df['TEAM']==team]
+        return pd.Series(index=list(s.columns), data=s.values.tolist()[0])
+
+def get_schedule(season, playoffs=False):
+    months = ['October', 'November', 'December', 'January', 'February', 'March',
+            'April', 'May']
+    df = pd.DataFrame()
+    for month in months:
+        r = get(f'https://www.basketball-reference.com/leagues/NBA_{season}_games-{month.lower()}.html')
+        if r.status_code==200:
+            soup = BeautifulSoup(r.content, 'html.parser')
+            table = soup.find('table', attrs={'id': 'schedule'})
+            month_df = pd.read_html(str(table))[0]
+            df = df.append(month_df)
+    df = df.reset_index()
+    cols_to_remove = [i for i in df.columns if 'Unnamed' in i]
+    cols_to_remove += [i for i in df.columns if 'Notes' in i]
+    cols_to_remove += [i for i in df.columns if 'Start' in i]
+    cols_to_remove += [i for i in df.columns if 'Attend' in i]
+    cols_to_remove += ['index']
+    df = df.drop(cols_to_remove, axis=1)
+    df.columns = ['DATE', 'VISITOR', 'VISITOR_PTS', 'HOME', 'HOME_PTS']
+    playoff_loc = df[df['DATE']=='Playoffs']
+    if len(playoff_loc.index)>0:
+        playoff_index = playoff_loc.index[0]
+    else:
+        playoff_index = len(df)
+    if playoffs:
+        df = df[playoff_index+1:]
+    else:
+        df = df[:playoff_index]
+    df['DATE'] = df['DATE'].apply(lambda x: pd.to_datetime(x))
+    return df
+
+def get_team_schedule(team, season, playoffs=False):
+    #MUST USE FULL TEAM NAME (ie 'Lakers' or 'Heat') NOT ABBREVIATION
+    total_schedule=get_schedule(season)
+    itt=int(0)
+    teams=[]
+    dates=[]
+    opponent=[]
+    points=[]
+    opp_points=[]
+    Rank=[]
+    home_away=[]
+
+
+    for index, row in total_schedule.iterrows():
+
+        if team in str(row['HOME']) and float(row['HOME_PTS'])>0:
+            teams.append(row['HOME'])
+            dates.append(row['DATE'])
+            opponent.append(row['VISITOR'])
+            points.append(row['HOME_PTS'])
+            opp_points.append(row['VISITOR_PTS'])
+            home_away.append('Home')
+            itt=itt+1
+            Rank.append(itt)
+
+        elif team in str(row['VISITOR']) and float(row['HOME_PTS'])>0:
+            teams.append(row['VISITOR'])
+            dates.append(row['DATE'])
+            opponent.append(row['HOME'])
+            points.append(row['VISITOR_PTS'])
+            opp_points.append(row['HOME_PTS'])
+            home_away.append('Away')
+            itt=itt+1
+            Rank.append(itt)
+
+
+
+
+    dataForDF={
+    'DATE':dates,
+    'Team': teams,
+    'Home/Away': home_away,
+    'Points': points,
+    'Opponent':opponent,
+    'Opponent Points':opp_points
+
+    }
+    team_schedule=pd.DataFrame(
+    dataForDF,
+    index=Rank)
+
+
+
+    return team_schedule
+
+def record_with_player(player, team):
+
+    start_date='2019-10-22'
+    end_date='2020-04-15'
+    player_game_log=get_game_logs(player,start_date,end_date)
+
+    player_games_played=player_game_log[['DATE','3P']]
+
+    player_games_played=player_games_played[player_games_played['3P']!='Inactive']
+    player_games_played=player_games_played[player_games_played['3P']!='Did Not Play']
+
+    player_games_played['DATE']=player_games_played['DATE'].astype(str)
+    team_schedule=get_team_schedule(team,2020)
+    team_schedule['DATE']=team_schedule['DATE'].astype(str)
+    commonGames = pd.merge(player_games_played, team_schedule, on=['DATE'], how='inner', sort=True)
+    winCount=0
+    lossCount=0
+    for index, row in commonGames.iterrows():
+        if float(row['Points'])>float(row['Opponent Points']):
+            winCount=winCount+1
+        else:
+            lossCount=lossCount+1
+    recordWPlayer=str(winCount)+'-'+str(lossCount)
+    return recordWPlayer
+
+def record_without_player(player, team):
+
+    start_date='2019-10-22'
+    end_date='2020-04-15'
+    player_game_log=get_game_logs(player,start_date,end_date)
+
+    player_games_played=player_game_log[['DATE','3P']]
+    date_game_missed=[]
+    itt=int(0)
+    Rank=[]
+
+    for index, row in player_games_played.iterrows():
+        if str(row['3P'])=='Inactive':
+            date_game_missed.append(row['DATE'])
+            itt=itt+1
+            Rank.append(itt)
+        elif str(row['3P'])=='Did Not Play':
+            date_game_missed.append(row['DATE'])
+            itt=itt+1
+            Rank.append(itt)
+
+    dataForDF={
+    'DATE':date_game_missed,
+
+    }
+    player_games_missed=pd.DataFrame(
+    dataForDF,
+    index=Rank)
+
+    player_games_missed['DATE']=player_games_missed['DATE'].astype(str)
+    team_schedule=get_team_schedule(team,2020)
+    team_schedule['DATE']=team_schedule['DATE'].astype(str)
+    uncommonGames = pd.merge(player_games_missed, team_schedule, on=['DATE'], sort=True)
+    winCount=0
+    lossCount=0
+    for index, row in uncommonGames.iterrows():
+        if float(row['Points'])>float(row['Opponent Points']):
+            winCount=winCount+1
+        else:
+            lossCount=lossCount+1
+    recordWPlayer=str(winCount)+'-'+str(lossCount)
+    return recordWPlayer
